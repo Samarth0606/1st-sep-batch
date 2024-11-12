cf3a732098043086907674f8e2920c48c3d8fbf5
Samarth Vohra <officialdevelopersamarth@gmail.com>
2024-11-12 20:03:59 +0530

    lecture 12 tic-tac-toe entirel

--- Lecture-12/tic-tac-toe/app.js
+++ Lecture-12/tic-tac-toe/app.js
@@ -6,11 +6,61 @@ for(let cell of cellElements){
     cell.addEventListener('click' , handleClick , {once:true} )
 }
 
+let circle_turn = false; // x 
 
-function handleClick(){
+let winning_contribution = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [0,4,8] 
+]
+
+function handleClick(e){
     // mark the symbol
+    let clickedCell = e.target;
+    let current_class = circle_turn ? "circle" : "x";
+    clickedCell.classList.add(current_class)
+
     // check for winner
+    if(checkWin(current_class)){
+        document.querySelector('.final-winner').innerHTML = `Winner ${current_class}`;
+        document.querySelector('.final-winner').parentElement.classList.add('show')
+    }
     // check draw
-    // swap the turn
-    
+    else if(isDraw()){
+         document.querySelector('.final-winner').innerHTML = 'Draw'
+         document.querySelector('.final-winner').parentElement.classList.add('show')
+    }
+    else{  
+        // swap the turn
+        circle_turn = !circle_turn;
+    }
 }
+
+
+function checkWin(current_class){
+    return winning_contribution.some((itemRow)=>{
+        return itemRow.every((item)=>{
+            return cellElements[item].classList.contains(current_class)
+        })
+    })
+}
+
+function isDraw(){
+    // spread operator
+    return [...cellElements].every((cell)=>{
+        return cell.classList.contains('x') || cell.classList.contains('circle')
+    })
+}
+
+
+document.querySelector('button').onclick = restart;
+
+function restart(){
+    window.location.reload()
+}
+
